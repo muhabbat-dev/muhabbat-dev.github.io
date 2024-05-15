d4e74f2f4e834bbf469f5cde5b593873aafbbe4e
Dev Mohib <dev.mohib@gmail.com>
2024-05-15 23:03:12 +0500

    fixed media:content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -9,15 +9,21 @@ export const GET = async(context) => {
     title: 'DevMohib’s Blog',
     description: 'An RSS feed to all my blogs',
     site:  context.url.origin,
+    xmlns: {
+      media: "http://search.yahoo.com/mrss/",
+      atom: "http://www.w3.org/2005/Atom",
+    },
     items : posts.map(post => ({
       title : post.frontmatter.title,
       pubDate : post.frontmatter.pubDate,
       description: post.frontmatter.description,
       link : `${context.url.origin}${post.url}`,
       author : "Muhabbat Ali",
-      enclosure : { type : "image/*", length:12345, 
-      url : `${context.url.origin}${post.frontmatter.image}`,
-    },
+      customData: `<media:content
+          type="image/*"
+          medium="image"
+          url="${context.url.origin}${post.frontmatter.image}" /> 
+      `,
     })),
     // (optional) inject custom xml
     customData: `<language>en-us</language>`,
