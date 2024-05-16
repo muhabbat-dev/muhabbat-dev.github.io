6a0eadd511a2743655c9c3e286927ba6f4b39c06
Dev Mohib <dev.mohib@gmail.com>
2024-05-16 12:00:10 +0500

    removed sanitizeHtml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -20,13 +20,12 @@ export const GET = async(context) => {
       description: post.frontmatter.description,
       link : `${context.url.origin}${post.url}`,
       author : "Muhabbat Ali",
-      content : sanitizeHtml(`
+      content :`
       <figure>
         <img class="webfeedsFeaturedVisual" alt="${post.frontmatter.title}" src="${context.url.origin}${post.frontmatter.image}" />
+        <p>${post.frontmatter.description}</p>
       </figure>
-    `,{
-      allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-    }),
+    `,
     enclosure: {
       length: 0,
       type :`image/${post.frontmatter.image.split(".")[1]}`,
@@ -46,7 +45,7 @@ export const GET = async(context) => {
     <language>en-us</language>
     <atom:link href="${context.url.origin}/rss.xml" rel="self" type="application/rss+xml" />
     <webfeeds:cover image="${context.url.origin}/assets/images/about.jpg" />
-    <webfeeds:accentColor>000000</webfeeds:accentColor>
+    <webfeeds:accentColor>00FF00</webfeeds:accentColor>
     <webfeeds:logo>${context.url.origin}/favicon.ico</webfeeds:logo>
     `,
   });
